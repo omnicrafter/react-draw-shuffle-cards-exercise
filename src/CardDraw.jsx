fd0781a2a08f92eb948db696fc984dbced3bbab6
Omnicrafter <chen.jeffrey.a@gmail.com>
2024-02-10 23:58:27 -0600

    added try/catch blocks to catch errors and prevent api call from running twice on page reload

--- src/CardDraw.jsx
+++ src/CardDraw.jsx
@@ -5,7 +5,13 @@ const CardDraw = () => {
   const [deckId, setDeckId] = useState(null);
   const [drawnCards, setDrawnCards] = useState([]);
   const [cardsLeftInDeck, setCardsLeftInDeck] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const hasFetched = useRef(false);
   const URL = "https://deckofcardsapi.com/api/deck";
+
+  useEffect(() => {
+    console.log(`Deck ID: ${deckId}`);
+  }, [deckId]);
 
   useEffect(() => {
     if (cardsLeftInDeck === 0) {
@@ -14,12 +20,24 @@ const CardDraw = () => {
   }, [cardsLeftInDeck]);
 
   const makeAndShuffleNewDeck = async () => {
-    const response = await fetch(`${URL}/new/shuffle/?deck_count=1`);
-    const data = await response.json();
-    setDeckId(data.deck_id);
-    setCardsLeftInDeck(data.remaining);
+    if (hasFetched.current) {
+      return;
+    }
+    hasFetched.current = true;
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${URL}/new/shuffle/?deck_count=1`);
 
-    setDrawnCards([]);
+      const data = await response.json();
+      setDeckId(data.deck_id);
+      setCardsLeftInDeck(data.remaining);
+
+      setDrawnCards([]);
+      hasFetched.current = false;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -30,20 +48,28 @@ const CardDraw = () => {
     if (cardsLeftInDeck === 0) {
       return alert("Error: no cards remaining! Use a new deck!");
     }
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${URL}/${deckId}/draw/?count=1`);
+      const data = await response.json();
 
-    const response = await fetch(`${URL}/${deckId}/draw/?count=1`);
-    const data = await response.json();
-
-    setDrawnCards((prevCards) => [...prevCards, data.cards[0].image]);
-    setCardsLeftInDeck(data.remaining);
-    console.log(cardsLeftInDeck);
+      setDrawnCards((prevCards) => [...prevCards, data.cards[0].image]);
+      setCardsLeftInDeck(data.remaining);
+      console.log(cardsLeftInDeck);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    setIsLoading(false);
   };
 
   return (
     <>
-      <button onClick={drawCard}>Draw a Card</button>
-      <button onClick={makeAndShuffleNewDeck}>New Deck</button>
-      <p>{!deckId ? "loading..." : deckId}</p>
+      <button onClick={drawCard} disabled={isLoading}>
+        Draw a Card
+      </button>
+      <button onClick={makeAndShuffleNewDeck} disabled={isLoading}>
+        New Deck
+      </button>
       <div className="card-container">
         {drawnCards.map((card, index) => (
           <Card key={index} imageUrl={card} />
