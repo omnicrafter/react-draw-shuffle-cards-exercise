980892fe866effb7f9f6491d6c7601b216d65b1b
Omnicrafter <chen.jeffrey.a@gmail.com>
2024-02-10 23:31:54 -0600

    completed main functionality of drawing cards and resetting to a new deck

--- src/CardDraw.jsx
+++ src/CardDraw.jsx
@@ -8,27 +8,43 @@ const CardDraw = () => {
   const URL = "https://deckofcardsapi.com/api/deck";
 
   useEffect(() => {
-    const makeAndShuffleNewDeck = async () => {
-      const response = await fetch(`${URL}/new/shuffle/?deck_count=1`);
-      const data = await response.json();
-      setDeckId(data.deck_id);
-    };
+    if (cardsLeftInDeck === 0) {
+      return alert("Error: no cards remaining!");
+    }
+  }, [cardsLeftInDeck]);
+
+  const makeAndShuffleNewDeck = async () => {
+    const response = await fetch(`${URL}/new/shuffle/?deck_count=1`);
+    const data = await response.json();
+    setDeckId(data.deck_id);
+    setCardsLeftInDeck(data.remaining);
+
+    setDrawnCards([]);
+  };
+
+  useEffect(() => {
     makeAndShuffleNewDeck();
   }, []);
 
   const drawCard = async () => {
+    if (cardsLeftInDeck === 0) {
+      return alert("Error: no cards remaining! Use a new deck!");
+    }
+
     const response = await fetch(`${URL}/${deckId}/draw/?count=1`);
     const data = await response.json();
-    const card = data.cards[0].image;
 
-    setDrawnCards((prevCards) => [...prevCards, card]);
+    setDrawnCards((prevCards) => [...prevCards, data.cards[0].image]);
     setCardsLeftInDeck(data.remaining);
+    console.log(cardsLeftInDeck);
   };
 
   return (
     <>
+      <button onClick={drawCard}>Draw a Card</button>
+      <button onClick={makeAndShuffleNewDeck}>New Deck</button>
       <p>{!deckId ? "loading..." : deckId}</p>
-      <div>
+      <div className="card-container">
         {drawnCards.map((card, index) => (
           <Card key={index} imageUrl={card} />
         ))}
